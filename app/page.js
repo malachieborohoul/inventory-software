349799f4f24f52aecb4c06077c4d1eba4a0d1b8d
malachieborohoul <malachieborohoul@gmail.com>
2024-01-03 00:28:49 +0100

    run eec

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import Image from 'next/image'
 // classNameclassNameclassNa
 export default function Home() {
   return (
-    <div className='fle'>
-
+    <div className='flex items-center justify-center min-h-screen'>
+      <h2 className='text-3xl'>Inventory Management Software</h2>
     </div>
   )
 }
