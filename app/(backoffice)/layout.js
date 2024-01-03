969e93577b5dd1e9eb2cf5b7a99f2dcb4f0ae8d2
malachieborohoul <malachieborohoul@gmail.com>
2024-01-03 01:06:24 +0100

    backoffice

---
+++ app/(backoffice)/layout.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Layout({children}) {
+  return (
+    <div className='bg-gray-300 min-h-screen'>{children}</div>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 // classNameclassNameclassNa
 export default function Home() {
   return (
-    <div className='flex items-center justify-center min-h-screen'>
+    <div className='flex items-center justify-center min-h-screen flex-col'>
       <h2 className='text-3xl'>Inventory Management Software</h2>
+      <Link href='/dashboard/home/dashboard'>Dashboard</Link>
     </div>
   )
 }
