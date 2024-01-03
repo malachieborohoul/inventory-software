123bdce46ec65db03554e29005c3a494c3a7ef0e
malachieborohoul <malachieborohoul@gmail.com>
2024-01-03 16:36:30 +0100

    Search

--- components/dashboard/Header.jsx
+++ components/dashboard/Header.jsx
@@ -1,8 +1,18 @@
+import { History } from 'lucide-react'
 import React from 'react'
+import SearchInput from './Search'
 
 const Header = () => {
   return (
-    <div className='bg-slate-300 '>Header</div>
+    <div className='bg-gray-100 h-12 flex justify-between items-center px-8'>
+        <div className='flex'>
+            <button><History className='h-5 w-5'/></button>
+            <SearchInput/>
+        </div>
+        <div className='flex'>
+            
+        </div>
+    </div>
   )
 }
 
