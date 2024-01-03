db7314bb92ffce54ed89648b0898a891135971e7
malachieborohoul <malachieborohoul@gmail.com>
2024-01-03 21:32:22 +0100

    ShoppingCart

--- components/dashboard/Sidebar.jsx
+++ components/dashboard/Sidebar.jsx
@@ -1,10 +1,15 @@
+import { ShoppingCart } from 'lucide-react'
 import React from 'react'
 
 const Sidebar = () => {
   return (
     <div className='w-64 min-h-screen bg-slate-900 text-slate-50'>
-        <div className='flex'>
-            
+        <div className='flex flex-col'>
+            <div className='flex '>
+                <ShoppingCart/>
+                <span>Inventory</span>
+            </div>
+
         </div>
       </div>
   )
