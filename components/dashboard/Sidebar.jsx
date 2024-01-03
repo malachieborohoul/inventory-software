584d4c663a32d38e5384a46cac7804051daa7fc6
malachieborohoul <malachieborohoul@gmail.com>
2024-01-03 21:30:16 +0100

    Sidebar

--- components/dashboard/Sidebar.jsx
+++ components/dashboard/Sidebar.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Sidebar = () => {
   return (
     <div className='w-64 min-h-screen bg-slate-900 text-slate-50'>
-        sidebar
+        <div className='flex'>
+            
+        </div>
       </div>
   )
 }
