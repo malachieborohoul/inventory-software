fcf1d44284f1bc5ecdfdf9b5b74a2f828a344065
malachieborohoul <malachieborohoul@gmail.com>
2024-01-03 21:33:15 +0100

    Inventory

--- components/dashboard/Sidebar.jsx
+++ components/dashboard/Sidebar.jsx
@@ -5,7 +5,7 @@ const Sidebar = () => {
   return (
     <div className='w-64 min-h-screen bg-slate-900 text-slate-50'>
         <div className='flex flex-col'>
-            <div className='flex '>
+            <div className='flex gap-3'>
                 <ShoppingCart/>
                 <span>Inventory</span>
             </div>
