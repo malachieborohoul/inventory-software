9bffc286142c77b79662b9aa7a059d37568eb8fa
malachieborohoul <malachieborohoul@gmail.com>
2024-01-03 21:14:50 +0100

    tailwind

--- components/dashboard/Search.jsx
+++ components/dashboard/Search.jsx
@@ -7,9 +7,9 @@ const SearchInput = () => {
         <label for="simple-search" class="sr-only">SearchInput</label>
         <div class="relative w-full">
             <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-               <Search />
+               <Search className='w-4 h-4 text-gray-500 dark:text-gray-500' />
             </div>
-            <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="SearchInput branch name..." required/>
+            <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search branch name..." required/>
         </div>
     </form>
   )
