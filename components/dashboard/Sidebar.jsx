6383387712b79b4ffbe0b0619c925f5ed757adb4
malachieborohoul <malachieborohoul@gmail.com>
2024-01-03 21:36:59 +0100

    Text xl

--- components/dashboard/Sidebar.jsx
+++ components/dashboard/Sidebar.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div className='w-64 min-h-screen bg-slate-900 text-slate-50'>
+    <div className='w-64 min-h-screen bg-slate-900 text-slate-50 p-3'>
         <div className='flex flex-col'>
-            <div className='flex gap-3'>
+            <div className='flex gap-3 '>
                 <ShoppingCart/>
-                <span>Inventory</span>
+                <span className='font-semibold text-xl'>Inventory</span>
             </div>
 
         </div>
