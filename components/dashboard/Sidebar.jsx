445b1547c97a75cb87a6733fdc67da44cc445ab8
malachieborohoul <malachieborohoul@gmail.com>
2024-01-04 09:58:22 +0100

    Sidebar

--- components/dashboard/Sidebar.jsx
+++ components/dashboard/Sidebar.jsx
@@ -1,4 +1,5 @@
-import { ShoppingCart } from 'lucide-react'
+import { ChevronLeft, Home, ShoppingCart } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 const Sidebar = () => {
@@ -7,7 +8,20 @@ const Sidebar = () => {
         <div className='flex flex-col'>
             <div className='flex gap-3 '>
                 <ShoppingCart/>
-                <span className='font-semibold text-xl'>Inventory</span>
+                <span className='font-semibold text-xl items-center bg-slate-950'>Inventory</span>
+            </div>
+
+            <nav className='flex flex-col gap-4'>
+                <Link className='flex' href="">
+                    <Home/>
+                    <span>Home</span>
+                </Link>
+            </nav>
+
+            <div className='flex flex-col justify-end '>
+                <div className='bg-slate-950 flex space-x-2 items-center py-3 px-2'>
+                    < ChevronLeft/>
+                </div>
             </div>
 
         </div>
