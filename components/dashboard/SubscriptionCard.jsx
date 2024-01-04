8afa4c5d6800955ca63beb44d6e63d7317c8223a
malachieborohoul <malachieborohoul@gmail.com>
2024-01-04 11:17:34 +0100

    SubscriptionCard

--- components/dashboard/SubscriptionCard.jsx
+++ components/dashboard/SubscriptionCard.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const SubscriptionCard = () => {
   return (
     <div className='p-3'>
-      <div className='px-3 mt-6 bg-slate-950 rounded-lg '>Subscribe 
-      <p>Your Premium plan's trial expires in <span className='text-orange-400 text-sm'>13 days</span> </p>
+      <div className='p-3 mt-6 bg-slate-950 rounded-lg '>Subscribe 
+      <p className='text-xs'>Your Premium plan's trial expires in <span className='text-orange-400 text-[0.5rem]'>13 days</span> </p>
       </div>
     </div>
   )
